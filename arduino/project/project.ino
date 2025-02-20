01c5169829bf91b2c03bcbe6de07ff002f7accc1
n1ndev <durrildev@gmail.com>
2025-02-20 14:47:37 +0000

    Trying local device architecture
    - Website that prompts the browser to connect to the QT Py

--- arduino/project/project.ino
+++ arduino/project/project.ino
@@ -2,15 +2,17 @@
 #include <ESPmDNS.h>
 #include <WebServer.h>
 
-const char* ssid = "ConditionsMesh";
-const char* password = "Conditions1189";
+// Replace with your WiFi credentials
+const char* ssid = "your_network_ssid";
+const char* password = "your_network_password";
 
-// The device will be accessible as http://qtpy.local
+// mDNS hostname – the device will be accessible as http://qtpy.local
 const char* hostname = "qtpy";
 
-// Pre-shared secret token
+// Pre-shared secret token for authentication
 const char* SECRET_TOKEN = "MY_SECRET_TOKEN";
 
+// Create a web server on port 80
 WebServer server(80);
 
 // Handler for the root URL (for testing)
@@ -19,14 +21,14 @@ void handleRoot() {
   server.send(200, "text/plain", "Hello from QT Py S3");
 }
 
-// Handler for /command?cmd=...&token=...
+// Handler for /command endpoint: expects "cmd" and "token" parameters.
 void handleCommand() {
+  // Check for token
   if (!server.hasArg("token")) {
     server.sendHeader("Access-Control-Allow-Origin", "*");
     server.send(401, "text/plain", "Token required");
     return;
   }
-  
   String token = server.arg("token");
   if (token != SECRET_TOKEN) {
     server.sendHeader("Access-Control-Allow-Origin", "*");
@@ -34,6 +36,7 @@ void handleCommand() {
     return;
   }
   
+  // Process command if provided
   if (server.hasArg("cmd")) {
     String cmd = server.arg("cmd");
     Serial.print("Received command: ");
@@ -49,9 +52,8 @@ void handleCommand() {
 void setup() {
   Serial.begin(115200);
   delay(1000);
-  Serial.println();
-
-  // Connect to WiFi network
+  
+  // Connect to WiFi
   Serial.print("Connecting to WiFi");
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
@@ -59,24 +61,22 @@ void setup() {
     Serial.print(".");
   }
   Serial.println();
-  Serial.println("WiFi connected");
-  Serial.print("IP address: ");
+  Serial.print("Connected. IP: ");
   Serial.println(WiFi.localIP());
-
-  // Start mDNS with the hostname "qtpy"
+  
+  // Start mDNS so the device is reachable as "qtpy.local"
   if (!MDNS.begin(hostname)) {
-    Serial.println("Error setting up MDNS responder!");
-    while (1) {
-      delay(1000);
-    }
+    Serial.println("Error starting mDNS");
+    while (1) { delay(1000); }
   }
   Serial.print("mDNS responder started: http://");
   Serial.print(hostname);
   Serial.println(".local");
-
-  // Setup web server routes
-  server.on("/", handleRoot);
-  server.on("/command", handleCommand);
+  
+  // Define HTTP server routes
+  server.on("/", HTTP_GET, handleRoot);
+  server.on("/command", HTTP_GET, handleCommand);
+  
   server.begin();
   Serial.println("HTTP server started");
 }
